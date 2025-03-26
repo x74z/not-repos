d61b0f0217db01146a31f9148919c93c1f8083d0
x74z <166341818+x74z@users.noreply.github.com>
2025-03-26 23:52:02 +0000

    Change to constructor

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,11 +1,12 @@
 import { LinkedList } from "./linked-list";
 
 class Hashmap {
-  capacity = 16;
-  loadFactor = 0.75;
-  buckets = [];
-  size = 0;
+  constructor(capacity = 16, loadFactor = 0.75) {
+    this.capacity = capacity;
+    this.loadFactor = loadFactor;
 
+    this.buckets = Array.apply(null, Array(capacity)).map(function() { });
+  }
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
@@ -17,10 +18,20 @@ class Hashmap {
   set(key, value) {
     // Should be [[key1,val1], [key2,val2], ...]
     // The hash returns a position inside the array.
+    // I will add them as regular values for now, later on to avoid
+    // collisions I'll make them Linked lists.
     const position = hash(key);
-    // Overwrite value if key is the same
+
+    if (buckets[position] !== key) {
+      // If there aren't used, add the key-value pair
+      const arrayToAdd = [key, value];
+      buckets[position] = arrayToAdd;
+    }
     if (buckets[position] === key) {
+      // Overwrite value if key is the same
       buckets[position][1] = value;
     }
   }
 }
+
+testyHashmap = new Hashmap();
