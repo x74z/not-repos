a5d610ecc868f4eebe540678dbecc54071d70424
x74z <166341818+x74z@users.noreply.github.com>
2025-03-31 23:14:53 +0100

    Add set, get and has methods to hashmap

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,11 +1,11 @@
-import { LinkedList } from "./linked-list";
+import { LinkedList } from "./linked-list.js";
 
 class Hashmap {
   constructor(capacity = 16, loadFactor = 0.75) {
     this.capacity = capacity;
     this.loadFactor = loadFactor;
 
-    this.buckets = Array.apply(null, Array(capacity)).map(function() { });
+    this.buckets = Array.apply(null, Array(capacity)).map(function () {});
   }
   hash(key) {
     let hashCode = 0;
@@ -13,25 +13,67 @@ class Hashmap {
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
-    return hashCode;
+    return hashCode % this.capacity;
   }
+
   set(key, value) {
     // Should be [[key1,val1], [key2,val2], ...]
     // The hash returns a position inside the array.
     // I will add them as regular values for now, later on to avoid
     // collisions I'll make them Linked lists.
-    const position = hash(key);
 
-    if (buckets[position] !== key) {
+
+    const position = this.hash(key);
+    const bucketAtKeyPosition = this.buckets[position];
+
+    if (this.buckets[position] !== null) {
       // If there aren't used, add the key-value pair
       const arrayToAdd = [key, value];
-      buckets[position] = arrayToAdd;
-    }
-    if (buckets[position] === key) {
+      this.buckets[position] = arrayToAdd;
+    } else if (this.buckets[position][0] === key) {
       // Overwrite value if key is the same
-      buckets[position][1] = value;
+      bucketAtKeyPosition[1] = value;
     }
   }
+
+  get(key) {
+    // takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null
+    const position = this.hash(key);
+    const bucketAtKeyPosition = this.buckets[position];
+
+    if (bucketAtKeyPosition !== undefined) {
+      return bucketAtKeyPosition[1];
+    }
+    return null;
+  }
+  has(key) {
+    // takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+    const position = this.hash(key);
+    const bucketAtKeyPosition = this.buckets[position];
+
+    if (
+      bucketAtKeyPosition !== undefined &&
+      bucketAtKeyPosition[0] === key
+    ) {
+      return true;
+    }
+    return false;
+  }
+  // NEXT TODO: actually use linked lists.
 }
 
-testyHashmap = new Hashmap();
+const test = new Hashmap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.get("lion"), test.has("lion"), test.has("fasfdsfaasdfsda"));
