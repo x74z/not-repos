d71203caadf3b8758586fed0878b8f1f7024316c
x74z <166341818+x74z@users.noreply.github.com>
2025-04-17 00:27:24 +0100

    add search tree balanced binary directory

---
+++ binary-search-trees/balanced-tree.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(value, leftValue, rightValue) {
+    this.value = value;
+    this.leftValue = leftValue;
+    this.rightValue = rightValue;
+  }
+}
+
+class Tree {
+  root = null;
+  constructor(arr){
+    this.arr=arr;
+  }
+
+  buildTree(arr, start, end) {
+
+  }
+}
