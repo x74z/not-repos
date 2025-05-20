c5d27d17b6614181b8bc54b866be56f17b8da4bd
x74z <166341818+x74z@users.noreply.github.com>
2025-05-20 22:35:44 +0100

    Add most of build tree, but doesnt work right

--- binary-search-trees/balanced-tree.js
+++ binary-search-trees/balanced-tree.js
@@ -1,18 +1,63 @@
 class Node {
-  constructor(value, leftValue, rightValue) {
-    this.value = value;
-    this.leftValue = leftValue;
-    this.rightValue = rightValue;
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class Tree {
   root = null;
-  constructor(arr){
-    this.arr=arr;
+  // constructor(arr) {
+  //   this.arr = arr;
+  // }
+  prettyPrint(node, prefix = "", isLeft = true) {
+    // Tip: If you would like to visualize your binary search tree, here is a prettyPrint() function that will console.log your tree in a structured format.
+    // This function will expect to receive the root of your tree as the value for the node parameter.
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false,
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 
   buildTree(arr, start, end) {
-
+    if (start > end) return null;
+    // console.log(arr);
+    const mid = Math.floor((start + end) / 2);
+    // Create the root node to add to its left and right values the subtrees
+    const root = new Node(arr[mid]);
+    // Create left tree
+    root.left = this.buildTree(arr, start, mid - 1);
+    // Create right tree
+    root.right = this.buildTree(arr, mid + 1, end);
+    return root;
+  }
+  buildTreeFromSortedArray(arr) {
+    return this.buildTree(arr, 0, arr.length - 1);
   }
 }
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const sortedAndNoDuplicatesArr = arr
+  .filter(function (item, pos) {
+    return arr.indexOf(item) == pos;
+  })
+  .sort((a, b) => a - b);
+console.log(sortedAndNoDuplicatesArr)
+const test = new Tree();
+const tree = test.buildTreeFromSortedArray(sortedAndNoDuplicatesArr);
+console.log(tree)
+test.prettyPrint(tree);
+
+
+
