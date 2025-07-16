73282c3fce75c344e2a56368681256e0a2893c89
x74z <166341818+x74z@users.noreply.github.com>
2025-07-16 11:08:28 +0100

    Clean up code and add todo

--- knight-travails/knight-travails.js
+++ knight-travails/knight-travails.js
@@ -8,7 +8,7 @@ function isCoordinateValid(coordinate) {
   }
   return false;
 }
-function getMovingPositionsForTheKnight(coordinate) {
+function getKnightPossibleMovingPositions(coordinate) {
   // When given a coordinate, it will output all the positions where the knight can move.
   const firstC = coordinate[0];
   const secondC = coordinate[1];
@@ -25,8 +25,10 @@ function getMovingPositionsForTheKnight(coordinate) {
 }
 function knightMoves(startingPos, endPos) {
   // So, as far as I understand, a valid moving position is [x+-1, y+-2] or [x+-2, y+-] as long as 0>=x,y<=7.
+  const possibleMoves = getKnightPossibleMovingPositions([startingPos]);
+  // TODO: figure out how to continue from here. 
 }
 
 // console.log(knightMoves([0, 0], [1, 2])); // Should output [0,0], [1,2].
 
-console.log(getMovingPositionsForTheKnight([0, 1]));
+console.log(getKnightPossibleMovingPositions([0, 1]));
